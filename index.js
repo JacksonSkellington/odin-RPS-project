8fc21e68820b164762367740b6053d8580efc737
JackSkellington <ahmadradwan08@gmail.com>
2024-05-12 13:17:42 +0300

    impl Human choice func

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ console.log("Hello, Odin!");
 
 let getComputerChoice = () => {
 	const move = ["Rock", "Paper", "Scissors"];
-	return move[Math.floor(Math.random() * 3)]
+	return move[Math.floor(Math.random() * 3)];
 }
 
+let getHumanChoice = (msg) => {
+	const move = ["rock", "paper", "scissors"];
+	let response = prompt(msg).toLowerCase();
+	
+	for (let i = 0; i < 3; i++) {
+		if (response === move[i]) {
+			return response;
+		}
+	}
 
+	return null;
+}
