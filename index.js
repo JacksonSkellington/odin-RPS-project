fcf1e6f6c7cbeb9fc30b982e2db476d4130e5dd7
JackSkellington <ahmadradwan08@gmail.com>
2024-05-12 17:58:34 +0300

    impl main game logic, fixed typo

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 console.log("Hello, Odin!");
 
+let humanScore = 0;
+let computerScore = 0;
+
 let getComputerChoice = () => {
-	const move = ["Rock", "Paper", "Scissors"];
+	const move = ["rock", "paper", "scissors"];
 	return move[Math.floor(Math.random() * 3)];
 }
 
@@ -15,5 +18,20 @@ let getHumanChoice = (msg) => {
 		}
 	}
 
+	return null;
+}
+
+let playRound = (humanChoice, computerChoice) => {
+}
+
+// This configuration assumes we're only checking if param1 beats param2.
+let judge = (humanChoice, computerChoice) => {
+	const move = ["rock", "paper", "scissors"]
+	// 'false' implies a loss for param1.
+	if (move[(humanChoice + 1) % 3] === move[computerChoice]) { return false; }
+	// 'true' implies a win for param1.
+	if (move[(humanChoice + 2) % 3] === move[computerChoice]) { return true; }
+
+	// 'null' implies a draw in this case.
 	return null;
 }
