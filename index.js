2e354720de2f90b85d24a522a3b7680cd097f398
JackSkellington <ahmadradwan08@gmail.com>
2024-05-19 17:11:26 +0300

    impl core round logic

--- index.js
+++ index.js
@@ -22,16 +22,27 @@ let getHumanChoice = (msg) => {
 }
 
 let playRound = (humanChoice, computerChoice) => {
+	let humanChoiceCapped = humanChoice.charAt(0).toUpperCase() + humanChoice.substring(1); 
+	let computerChoiceCapped = computerChoice.charAt(0).toUpperCase() + computerChoice.substring(1); 
+	let outcome = judge(humanChoice, computerChoice);
+
+	if ( outcome === null) { console.log("It's A Draw! no points awarded.") }
+	else if (outcome === false) { console.log(`You Lose! ${computerChoiceCapped} beats ${humanChoiceCapped}.`) }
+	else { console.log(`You Win! ${humanChoiceCapped} beats ${computerChoiceCapped}.`)}
 }
 
 // This configuration assumes we're only checking if param1 beats param2.
 let judge = (humanChoice, computerChoice) => {
 	const move = ["rock", "paper", "scissors"]
+	let humanChoiceIndex = move.indexOf(humanChoice);
 	// 'false' implies a loss for param1.
-	if (move[(humanChoice + 1) % 3] === move[computerChoice]) { return false; }
+	if (move[(humanChoiceIndex + 1) % 3] === computerChoice) { return false; }
 	// 'true' implies a win for param1.
-	if (move[(humanChoice + 2) % 3] === move[computerChoice]) { return true; }
+	if (move[(humanChoiceIndex + 2) % 3] === computerChoice) { return true; }
 
 	// 'null' implies a draw in this case.
+	// TODO: Use something other than 'null'
 	return null;
 }
+
+playRound(getHumanChoice("Pick your move: "), getComputerChoice());
